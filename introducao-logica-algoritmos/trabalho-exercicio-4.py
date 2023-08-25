e88ac8497781a4662932d3777b25f4c18f407058
Gah2k0 <gah.francisco@gmail.com>
2023-08-25 17:10:07 -0300

    listagem, remoção e menu principal

--- introducao-logica-algoritmos/trabalho-exercicio-4.py
+++ introducao-logica-algoritmos/trabalho-exercicio-4.py
@@ -12,10 +12,21 @@
 lista_colaboradores = []
 id_global = 0
 
+def formatar_print(colaborador):
+    print('Dados do Colaborador: \n'
+          'ID: %s \n'
+          'NOME: %s \n'
+          'SETOR: %s \n'
+          'REMUNERAÇÃO: R$ %s \n' %(colaborador['id'], colaborador['nome'], colaborador['setor'], colaborador['pagamento']))
+
+
 def cadastrar_colaborador(id):
+    print("--------------------------------MENU DE CADASTRO-----------------------------------")
     nome = input('Informe o nome do novo colaborador: ')
     setor = input('Informe o setor do novo colaborador: ')
     pagamento = input('Informe o valor de pagamento do novo colaborador (R$): ')
+    print("-----------------------------------------------------------------------------------")
+    print("***********************************************************************************")
 
     colaborador = {
         'id': id,
@@ -24,6 +35,63 @@ def cadastrar_colaborador(id):
         'pagamento': pagamento
     }
     lista_colaboradores.append(colaborador)
+    global id_global
+    id_global += 1
 
-cadastrar_colaborador(1)
-print(lista_colaboradores)
+def consultar_colaborador():
+    opcao = input('Escolha a opção desejada: \n'
+          '1. Consultar todos\n'
+          '2. Consultar por Id\n'
+          '3. Consultar por Setor\n'
+          '4. Retornar ao menu principal\n'
+          '>> ')
+    if opcao == '1':
+        for colaborador in lista_colaboradores:
+            formatar_print(colaborador)
+    elif opcao == '2':
+        id = input('por favor digite o Id do colaborador: ')
+        formatar_print(lista_colaboradores[int(id)])
+    elif opcao == '3':
+        setor = input('por favor digite o setor desejado: ')
+        for colaborador in lista_colaboradores:
+            if colaborador.get('setor') == setor:
+                formatar_print(colaborador)
+
+def remover_colaborador():
+    id = input('por favor digite o Id do colaborador que deseja remover: ')
+    try:
+        lista_colaboradores.pop(int(id))
+        print('Colaborador removido com sucesso.')
+    except IndexError:
+        print('Desculpe, esse Id é inválido, tente novamente')
+
+
+#cadastrar_colaborador(1)
+#print(lista_colaboradores)
+
+print("Bem-vindo ao Sistema de Cadastro do Gabriel Francisco")
+while(True):
+    print("---------------------------------MENU PRINCIPAL------------------------------------")
+    opcao = input('O que você deseja fazer: \n'
+          '1. Cadastrar Colaborador\n'
+          '2. Consultar Colaborador\n'
+          '3. Remover um Colaborador\n'
+          '4. Sair\n'
+          '>> ')
+    print("-----------------------------------------------------------------------------------")
+    print("***********************************************************************************")
+
+    if opcao == '1':
+        cadastrar_colaborador(id_global)
+    elif opcao == '2':
+        try:
+            consultar_colaborador()
+        except IndexError:
+            print('Desculpe, esse Id é inválido, tente novamente')
+    elif opcao == '3':
+        remover_colaborador()
+    elif opcao == '4':
+        break
+    else:
+        print('Opção inválida! Tente novamente.')
+        continue
